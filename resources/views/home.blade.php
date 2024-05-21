54a209ab3ffa6e26c4fd982d602882bc1c1703f7
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-21 17:03:20 +0200

    cards created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,27 @@
 @section('content')
 <section class="container">
     <h1>Movies</h1>
+       
+    <div class="row row-cols-1 row-cols-lg-4 g-4">
     @foreach ($movies as $movie)
-    <ul>
-    <li>{{$movie->title}}</li>
-    </ul>
+        <div class="col">             
+            <div class="card">
+                <img src="{{$movie->image}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie->title}}</h5>
+                    <p class="card-text">Vote: {{$movie->vote}}</p>
+                </div>
+            </div>
+            
+        </div>
+        @endforeach
+    </div>
+
     
     
-    @endforeach
+    
+
+    
 </section>
 
 @endsection
